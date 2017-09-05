b90a5e33db6979cdaf771a0c20a08f38d3091294
java <java@bigdata>
2017-09-05 09:40:50 +0900

    first commit

---
+++ hello.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env scala 
+2 
+ 
+3 for (i <- 0 until 10) { 
+4    if ( i % 2 == 1){ 
+5       println("########"); 
+6       println("i =" + i); 
+7       println("test end...");    
+8    } 
+9 } 
