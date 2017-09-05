55e926211b5b12876ca6c5e2b1c08b9815699525
java <java@bigdata.com>
2017-09-05 10:18:11 +0900

    modify

--- hello.sh
+++ hello.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env scala 
-2 
+
  
-3 for (i <- 0 until 10) { 
-4    if ( i % 2 == 1){ 
-5       println("########"); 
-6       println("i =" + i); 
-7       println("test end...");    
-8    } 
-9 } 
+ for (i <- 0 until 10) { 
+    if ( i % 2 == 1){ 
+       println("########"); 
+       println("i =" + i); 
+       println("test end...");    
+       println("test end...");    
+    } 
+ } 
